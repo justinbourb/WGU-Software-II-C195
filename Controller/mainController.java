5617aee55f072c9455042b5cf4e8f65c44bc47c3
justinbourb <30423126+justinbourb@users.noreply.github.com>
2020-12-08 17:57:30 -0500

    todo

--- Controller/mainController.java
+++ Controller/mainController.java
@@ -36,13 +36,13 @@ public class mainController  implements Initializable {
     /*Requirements:
     1.  Create a log-in form with the following capabilities:
 
-•  accepts a user ID and password and provides an appropriate error message
+incomplete - open next gui page after login •  accepts a user ID and password and provides an appropriate error message
 
-•  determines the user’s location (i.e., ZoneId) and displays it in a label on the log-in form
+incomplete - show as label on gui •  determines the user’s location (i.e., ZoneId) and displays it in a label on the log-in form
 
-•  displays the log-in form in English or French based on the user’s computer language setting to translate all the text, labels, buttons, and errors on the form
+ complete •  displays the log-in form in English or French based on the user’s computer language setting to translate all the text, labels, buttons, and errors on the form
 
-•  automatically translates error control messages into English or French based on the user’s computer language setting
+incomplete - show as label on gui •  automatically translates error control messages into English or French based on the user’s computer language setting
 */
 
     /**
