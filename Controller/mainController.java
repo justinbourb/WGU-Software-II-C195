4a24c4f2f890db6d9562edcb571c31f0c4f13cd6
justinbourb <30423126+justinbourb@users.noreply.github.com>
2020-12-11 17:34:43 -0500

    Update mainController.java

--- Controller/mainController.java
+++ Controller/mainController.java
@@ -13,13 +13,75 @@ import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import Helpers.switchStage;
 import DAO.read;
 
 import static DAO.read.readData;
 
-/** This class controls the mainView.fxml */
-public class mainController {
-  public void submitButtonAction(ActionEvent actionEvent)
+/* TODO
+*1) access DB logic
+*2) Create customers tableview
+*3) etc?
+*/
 
+
+/** This class controls the mainView.fxml */
+public class mainController implements Initializable {
+    /** This method opens customersView.fxml */
+    @FXML
+    void addCustomersButtonAction(ActionEvent actionEvent) throws IOException {
+        String resourceURL = "/View/customersView.fxml";
+        switchStage.switchStage(actionEvent, resourceURL);
+    }
+  
+    /** This method opens appointmentsView.fxml */
+    @FXML
+    void addAppointmentsButtonAction(ActionEvent actionEvent) throws IOException {
+        String resourceURL = "/View/appointmentsView.fxml";
+        switchStage.switchStage(actionEvent, resourceURL);
+    }
+  
+    /** This method deletes a customer. */
+    @FXML
+    void deleteCustomersButtonAction(ActionEvent event) throws IOException {
+        String confirmText = "Pressing ok will delete this customer.";
+        String resourceURL = "/View/mainView.fxml";
+        boolean wasOkPressed = ConfirmView.showAlert(confirmText);
+        if (wasOkPressed){
+            //DELETE CUSTOMER
+        }
+    }
+  
+    /** This method deletes an appointment. */
+    @FXML
+    void deleteAppointmentButtonAction(ActionEvent event) throws IOException {
+        String confirmText = "Pressing ok will delete this customer.";
+        String resourceURL = "/View/mainView.fxml";
+        boolean wasOkPressed = ConfirmView.showAlert(confirmText);
+        if (wasOkPressed){
+            //DELETE APPOINTMENT
+        }
+    }
+  
+    /** This method opens loginView.fxml */
+    @FXML
+    void logoutButtonAction(ActionEvent actionEvent) throws IOException {
+        String resourceURL = "/View/loginView.fxml";
+        switchStage.switchStage(actionEvent, resourceURL);
+    }
+
+    /** This method opens calendarView.fxml */
+    @FXML
+    void viewAppointmentsButtonAction(ActionEvent actionEvent) throws IOException {
+        String resourceURL = "/View/calendarView.fxml";
+        switchStage.switchStage(actionEvent, resourceURL);
+    }
+  
+    /**This function is automatically called by Java.  
+    It handles data setup for the GUI to display.
+    */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    //fill customer table view
 
 }
