f8a647d4177264a9e7f30d352b513cd48848a517
justinbourb <30423126+justinbourb@users.noreply.github.com>
2020-12-11 17:14:04 -0500

    Create mainController.java

---
+++ Controller/mainController.java
@@ -0,0 +1,25 @@
+  
+package Controller;
+
+import com.sun.tools.jconsole.JConsoleContext;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+
+import java.io.DataOutput;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import DAO.read;
+
+import static DAO.read.readData;
+
+/** This class controls the mainView.fxml */
+public class mainController {
+  public void submitButtonAction(ActionEvent actionEvent)
+
+
+}
