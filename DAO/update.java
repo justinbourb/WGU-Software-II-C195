5210776507294eaee0265e818c5cc48883b0a8ae
justinbourb <30423126+justinbourb@users.noreply.github.com>
2020-12-16 16:28:10 -0500

    Update update.java

--- DAO/update.java
+++ DAO/update.java
@@ -1,4 +1,28 @@
 package DAO;
 
+/**This class is part of the Database Access Object design pattern. 
+* It is used to update data from the database.*/
+
 public class update {
+  
+    /**This function updates data from the database and returns the results as a ResultSet.
+    * @param column, the column name as a string
+    * @param table, the table as a string
+    * @param where, the where as a string
+    * @exception SQLException, an SQL Exception
+    * @exception ClassNotFoundException, a Class Not Found Exception
+    * @return results, a ResultsSet of the data found
+    */
+    public static ResultSet readData(String column, String table, String where) throws SQLException, ClassNotFoundException {
+        /* Example usage:
+        UPDATE Customers
+        SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+        WHERE CustomerID = 1;
+        */
+        Connection connection = connect.startConnection();
+        PreparedStatement query = connection.prepareStatement("UPDATE " + column + " SET " + table + " WHERE " + where);
+        ResultSet results = query.executeQuery();
+        return results;
+    }
+}
 }
