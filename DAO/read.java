34b722161a706857405cff36c0c93d5d8e615703
justinbourb <30423126+justinbourb@users.noreply.github.com>
2020-12-16 15:44:43 -0500

    add javadoc commentary

--- DAO/read.java
+++ DAO/read.java
@@ -5,8 +5,18 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**This class is part of the Database Access Object design pattern. 
+* It is used to read data from the database.*/
 public class read {
 
+    /**This function reads data from the database and returns the results as a ResultSet.
+    * @param column, the column name as a string
+    * @param table, the table as a string
+    * @param where, the where as a string
+    * @exception SQLException, an SQL Exception
+    * @exception ClassNotFoundException, a Class Not Found Exception
+    * @return results, a ResultsSet of the data found
+    */
     public static ResultSet readData(String column, String table, String where) throws SQLException, ClassNotFoundException {
         Connection connection = connect.startConnection();
         PreparedStatement query = connection.prepareStatement("SELECT " + column + " FROM " + table + " WHERE " + where);
