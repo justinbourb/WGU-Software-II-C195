c3be2213ef350c7db82c709cec25721c832a9d00
justinbourb <30423126+justinbourb@users.noreply.github.com>
2020-12-17 17:21:40 -0500

    overload readData

--- DAO/read.java
+++ DAO/read.java
@@ -21,4 +21,18 @@ public class read {
         PreparedStatement query = connection.prepareStatement("SELECT " + column + " FROM " + table + " WHERE " + where);
         return query.executeQuery();
     }
+    
+    /**This function reads data from the database and returns the results as a ResultSet.
+    * This overload function is for all results (no WHERE clause).
+    * @param column, the column name as a string
+    * @param table, the table as a string
+    * @param where, the where as a string
+    * @exception SQLException, an SQL Exception
+    * @return results, a ResultsSet of the data found
+    */
+    public static ResultSet readData(String column, String table) throws SQLException {
+        Connection connection = connect.startConnection();
+        PreparedStatement query = connection.prepareStatement("SELECT " + column + " FROM " + table);
+        return query.executeQuery();
+    }
 }
